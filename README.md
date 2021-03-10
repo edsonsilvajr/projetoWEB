e6dfbd843f7f2a9018306b90320b9727d70aa0d4
Edson Luiz <edsonsilvajr21@gmail.com>
2021-03-10 12:34:23 -0300

    adicionado componente para lidar com controle de login (usuário logado e deslogado)

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # projetoWEB
 
-oi
+Esse repositório contém o projeto para a disciplina de Desenvolvimento WEB - servidor
+
+# Conteúdo
+* Frontend: realizado com React e Typescript.
+* Backend: Iniciado com PHP básico, depois utilizado Laravel.
+
+# Como Rodar o projeto
+Para poder rodar o projeto localmente na sua máquina, é necessário ter o PHP e Apache instalado, recomendamos o uso do xampp que já é um pacote que abranje tudo o que será necessário
+
+# Rodando o frontend
+Ler readme da pasta frontend
+
+# Rodando o backend
+Ler readme da pasta backend
--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,27 +1,111 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './styles.scss'
 import logo from '../../assets/logo-icon.svg'
+import userIcon from '../../assets/user-icon.svg'
 import { Link } from 'react-router-dom'
 
+interface Props {
+  condition: boolean
+}
+
+interface RefObject<T> {
+  // immutable
+  readonly current: T | null
+}
+
 const Navbar = () => {
-  const teste = () => {
-    console.log('feito')
+  const [condition, setCondition] = useState(false)
+  const [modalOpen, setModalOpen] = useState(false)
+
+  const useOutsideClickEvent = (ref: RefObject<any>) => {
+    useEffect(() => {
+      const handleClickOutside = (event: Event) => {
+        if (ref.current && !ref.current.contains(event.target)) {
+          console.log(ref)
+          setModalOpen(!modalOpen)
+        }
+      }
+
+      document.addEventListener('mousedown', handleClickOutside)
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutside)
+      }
+    }, [ref])
   }
+
+  const UserSettings = () => {
+    const wrapperRef = useRef(null)
+    useOutsideClickEvent(wrapperRef)
+
+    return (
+      <div ref={wrapperRef} className="user-settings">
+        <p>Favoritos</p>
+        <p>Gerenciar Receitas</p>
+        <p>Exibir Perfil</p>
+        <Link
+          to="/"
+          className="log-out"
+          onClick={() => {
+            setModalOpen(false)
+            setCondition(!condition)
+          }}
+        >
+          Logout
+        </Link>
+      </div>
+    )
+  }
+
+  const UserControl = ({ condition }: Props) => {
+    if (condition) {
+      return (
+        <div className="auth-wrapper">
+          <Link
+            to="/"
+            className="log-in"
+            onClick={() => {
+              setCondition(!condition)
+            }}
+          >
+            Log in
+          </Link>
+          <Link to="/" className="sign-in">
+            Sign in
+          </Link>
+        </div>
+      )
+    } else {
+      return (
+        <div className="user-wrapper">
+          <div
+            className="user"
+            onMouseEnter={() => {
+              setModalOpen(true)
+            }}
+            onClick={() => {
+              if (!modalOpen) setModalOpen(true)
+            }}
+          >
+            <img src={userIcon} alt="user-icon" />
+            <div className="user-info">
+              <p>User</p>
+              <p>User Type</p>
+            </div>
+          </div>
+          {modalOpen && <UserSettings />}
+        </div>
+      )
+    }
+  }
+
   return (
     <div className="navbar-header-container">
-      <Link to="/" className="logo" onClick={teste}>
+      <Link to="/" className="logo">
         <img src={logo} alt="logo" className="logo-icon" />
         <p>RANDOM KITCHEN</p>
       </Link>
       <div className="search-bar">search</div>
-      <div className="auth-wrapper">
-        <Link to="/" className="log-in">
-          Log in
-        </Link>
-        <Link to="/" className="sign-in">
-          Sign in
-        </Link>
-      </div>
+      <UserControl condition={condition} />
     </div>
   )
 }
