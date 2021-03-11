5c2619103d59d69c8f10004bdf905adf374320bb
Edson Luiz <edsonsilvajr21@gmail.com>
2021-03-11 12:37:59 -0300

    adicionados componentes as suas respectivas rotas

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
 import Recipe from './pages/Recipe'
 
 const Routes = () => {
@@ -9,6 +10,7 @@ const Routes = () => {
       <Navbar />
       <Route component={Dashboard} path="/" exact />
       <Route component={Recipe} path="/recipe/:recipe_id+" />
+      <Route component={Login} path="/login" />
     </BrowserRouter>
   )
 }
