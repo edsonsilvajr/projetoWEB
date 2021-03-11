e037a31f3297a7c95343f54066df8b7800c8da66
dpavlak <davidpavlak@hotmail.com>
2021-03-11 15:03:36 -0300

    Correções

---
+++ frontend/src/pages/CadastroEdit/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Background from '../../components/Background'
+import CadastroForm from '../../components/CadastroForm'
+
+function CadastroEdit() {
+    return (
+        <Background background="background-option1 center">
+        <CadastroForm></CadastroForm>
+        </Background>
+    )
+}
+
+export default CadastroEdit
